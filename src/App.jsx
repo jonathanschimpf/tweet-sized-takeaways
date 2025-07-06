ead8b23a2b3e58f0a5cc71185d04b48cd079e905
jonathanschimpf <jonathan@jonathanschimpf.com>
2025-07-05 21:01:06 -0400

    twitter-died-jetblack.png looks better

--- src/App.jsx
+++ src/App.jsx
@@ -136,7 +136,7 @@ function App() {
                   <FontAwesomeIcon icon={copied ? faTwitter : faCopy} />
                 </button>
                 <img
-                  src="/images/twitter-died.png"
+                  src="/images/twitter-died-jetblack.png"
                   alt="Dead Twitter bird"
                   className="black-deadtwitterbird"
                 />
