768cd41a5631ee19a573d2d1e400935cdb238d6e
jonathanschimpf <jonathan@jonathanschimpf.com>
2025-07-22 14:09:34 -0400

    app.add_middleware(     CORSMiddleware,

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://tweet-sized-takeaways.netlify.app"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
